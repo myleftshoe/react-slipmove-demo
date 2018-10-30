fa2cd1809c9d6274faddaeac37d62d8356a2d4f4
Paul <paulesammut@gmail.com>
2018-10-30 13:17:55 +1100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, PureComponent } from 'react';
+import SlipMove from 'react-slipmove';
 import './App.css';
+import { move, shuffle, reverse } from './array';
 
-class App extends Component {
+const generateItems = length => [...Array(length).keys()].map(k => `Item ${k}`)
+
+const actions = {
+  reverse: ({items}) => ({items: reverse(items)}),
+  shuffle: ({items}) => ({items: shuffle(items)}),
+  move: (oldIndex, newIndex) => ({items}) => ({items: move(items, oldIndex, newIndex)}),
+}
+
+export default class extends Component {
+
+  state = { items: generateItems(20) }
+  
+  move = ({oldIndex, newIndex}) => this.setState(actions.move(oldIndex, newIndex));
+  reverse = () => this.setState(actions.reverse);
+  shuffle = () => this.setState(actions.shuffle);
+
   render() {
+    const { items } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" >
+        <button onClick={this.reverse}>Reverse</button>
+        <button onClick={this.shuffle}>Shuffle</button>
+        <SlipMove onMoveEnd={this.move} flipMoveProps={{appearAnimation: 'fade'}} >
+          {items.map(item => <ListItem key={item}>{item}</ListItem>)}
+        </SlipMove>
       </div>
     );
   }
 }
 
-export default App;
+// FlipMove requires class compoonents as children
+class ListItem extends PureComponent {
+  render() {
+    const { children } = this.props;
+    return <div className="list-item" >{children}</div>
+  }
+}
---
+++ src/array.js
@@ -0,0 +1,24 @@
+export const move = (arr, removeFrom=null, insertAt=null) => {
+    /*
+        Adds item if removeFrom is null
+        Removes item if insertAt is null
+    */
+    if (insertAt === removeFrom) return arr;
+    if (isNaN(Number(removeFrom) || isNaN(Number(insertAt)))) return arr;
+
+    let _arr = [...arr];
+
+    const item = (removeFrom !== null) ? _arr.splice(removeFrom, 1)[0] : null; 
+    if (insertAt !== null) {
+        _arr.splice(insertAt, 0, item);
+    }
+
+    return _arr;
+  };
+    
+export const shuffle = arr => arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1]);
+
+export const reverse = arr => arr.slice().reverse();
